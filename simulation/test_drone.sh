bac1e074ed3266ab558236e0112a9bfc6bb87988
KocurMaros <kocur.maros@gmail.com>
2026-04-15 17:00:45 +0200

    Add test script for takeoff drone

---
+++ simulation/test_drone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Setting mode to GUIDED..."
+ros2 service call /mavros/set_mode mavros_msgs/srv/SetMode "{base_mode: 0, custom_mode: 'GUIDED'}"
+sleep 2
+
+echo "Arming drone..."
+ros2 service call /mavros/cmd/arming mavros_msgs/srv/CommandBool "{value: True}"
+sleep 2
+
+echo "Taking off..."
+ros2 service call /mavros/cmd/takeoff mavros_msgs/srv/CommandTOL "{min_pitch: 0, yaw: 90, altitude: 2}"
+echo "Takeoff command sent!"
